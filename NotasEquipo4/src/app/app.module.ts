0d411e31258c601422a66f44e811ee8b675ff91c
Tomas Flores Garcia <tfloresgarcia94@gmail.com>
2021-08-10 12:20:11 -0500

    Agregando el login

--- NotasEquipo4/src/app/app.module.ts
+++ NotasEquipo4/src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AppRoutingService } from './app-routing.service';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingService
   ],
   providers: [],
   bootstrap: [AppComponent]
