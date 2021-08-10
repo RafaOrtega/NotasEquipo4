7451aea263d8ea0a2f76c266c9e4546e8b1d60db
Ezequiel Macías Rivera <ezequielmr678@gmail.com>
2021-08-10 13:04:43 -0500

    gregué componente notas

--- NotasEquipo4/src/app/app.module.ts
+++ NotasEquipo4/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NotasComponent } from './notas/notas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotasComponent
   ],
   imports: [
     BrowserModule
