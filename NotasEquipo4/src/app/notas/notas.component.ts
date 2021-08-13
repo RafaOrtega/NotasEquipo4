2fdf79d32f0b62a02690e9604e177b9660b220f7
ALMARAZ SIERRA <cintiaalm.25@gmail.com>
2021-08-13 16:54:01 -0500

    Metodo para eliminar una nota

--- NotasEquipo4/src/app/notas/notas.component.ts
+++ NotasEquipo4/src/app/notas/notas.component.ts
@@ -24,4 +24,22 @@ async getNotee(){
   }
 }
 
+  public eliminarNota(nota: Note){
+    console.log(nota.emailUser);
+    console.log(this.notes);
+    if(nota.emailUser==localStorage.emailLogged){
+      if(confirm("Seguro de borrar la nota? ")) {
+        let notapos= this.notes.indexOf(nota); 
+        console.log(notapos);  
+        //this.notes.splice(notapos,1);
+        //let notasArray=JSON.parse(localStorage.notes);
+       // localStorage.removeItem("notes");
+        JSON.parse(localStorage.notes).splice(notapos,1);
+        console.log(this.notes);
+      }
+    }else{
+      alert("No puedes eliminar esta nota")
+    }
+  }
+
 }
